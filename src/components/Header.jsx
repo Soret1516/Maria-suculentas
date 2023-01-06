4944d1ec2f0dd049e8a9645ac80c28380f4dc7d7
alejandro soret <alejandro-soret1516@hotmail.com>
2023-01-05 23:26:18 -0500

    header, footer, logo, main, css styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/Img/planta.jpg"
+export function Header() {
+    return (
+    <header>
+        <img src={logo} alt="planta" id='logo'/>
+     Maria suculentas SAS | Inicio 
+      </header>
+    )
+}
+
